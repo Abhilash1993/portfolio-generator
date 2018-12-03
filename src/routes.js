c53b5f271905ce57b9344c57deb94feb456d72cb
Rahul Deshpande <rahuldeshpande@Rahuls-MBP-2.fios-router.home>
2018-12-03 00:36:06 -0500

    UI fixes and modal pop up along with template 2 api integration

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,13 @@ import Dummy from './dummy/Dummy';
 import Generate from './generate-portfolio/generate-portfolio';
 import Template1 from './home/templates/template-1/template-1';
 import Template2 from './home/templates/template-2/template-2';
+import ModalPopup from './common/components/modal-popup/modalPopup';
 
 const routes = (
     <div>
       <Route exact path='/' component={Home} />
       <Route exact path='/about' component={About} />
-      <Route exact path='/dummy' component={Dummy} />
+      {/* <Route exact path='/dummy' component={ModalPopup} /> */}
       <Route exact path='/generate' component={Generate} />
       <Route exact path='/template-1' component={Template1}/>
       <Route exact path='/template-2' component={Template2}/>
